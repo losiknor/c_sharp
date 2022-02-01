bc10b45aeb6f8a7c76213e6d8437b18ee7d14bbb
Larina <Losiknor@gmail.com>
2022-02-01 22:43:39 +0700

    Набран текст программы 7 задачи

---
+++ 7 zadacha/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать число от -N до N
+Console.Write("Введите число N: ");
+int maxN = Console.ReadLine();
+
+int minN = -maxN;
+while (minN <= maxN)
+{
+    Console.WriteLine(minN);
+    minN ++
+}
